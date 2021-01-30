448d57c3bf8ac7acafe0e82e254a548cf5689ba5
Cornel Ghiban <cghiban@gmail.com>
2021-01-30 08:05:49 -0500

    display the entries as a regular table on iPads

--- var/static/scripts.js
+++ var/static/scripts.js
@@ -49,20 +49,20 @@ document.querySelector('button').addEventListener('click', (ev) => {
 
     async function postData(url = '', data = {}) {
         // Default options are marked with *
-    const response = await fetch(url, {
-        method: 'POST', // *GET, POST, PUT, DELETE, etc.
-        mode: 'cors', // no-cors, *cors, same-origin
-        cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-        credentials: 'same-origin', // include, *same-origin, omit
-        headers: {
-        'Content-Type': 'application/json'
-        // 'Content-Type': 'application/x-www-form-urlencoded',
-        },
-        redirect: 'follow', // manual, *follow, error
-        referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-        body: JSON.stringify(data) // body data type must match "Content-Type" header
-    });
-    return response.json(); // parses JSON response into native JavaScript objects
+        const response = await fetch(url, {
+            method: 'POST', // *GET, POST, PUT, DELETE, etc.
+            mode: 'cors', // no-cors, *cors, same-origin
+            cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+            credentials: 'same-origin', // include, *same-origin, omit
+            headers: {
+            'Content-Type': 'application/json'
+            // 'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            redirect: 'follow', // manual, *follow, error
+            referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+            body: JSON.stringify(data) // body data type must match "Content-Type" header
+        });
+        return response.json(); // parses JSON response into native JavaScript objects
     };
     let data = {
         reader: formData.get("reader"),
@@ -72,11 +72,11 @@ document.querySelector('button').addEventListener('click', (ev) => {
         duration: parseInt(formData.get("duration"),10),
     };
     console.log(data);
-    postData('/add?a=1', data) // new FormData(form)
+    postData('/add', data)
     .then(data => {
         console.log(data); // JSON data parsed by `data.json()` call
         if (data && data.status === "ok") {
-            document.location.href = "/";
+            document.location.href = "/?reader=" + formData.get("reader");
         }
     });
 
