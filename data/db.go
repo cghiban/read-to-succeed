5d196cc9c5b1c78ed32157d663189e50b61bbef5
Cornel Ghiban <cghiban@gmail.com>
2022-02-03 20:51:29 -0500

    admins may see readers in a group

--- data/db.go
+++ data/db.go
@@ -50,9 +50,10 @@ type Reading struct {
 
 // Reader - type for handling readers
 type Reader struct {
-	ID     int    `json:"id,omitempty"`
-	UserID int    `json:"user_id,omitempty"`
-	Name   string `json:"name"`
+	ID        int       `json:"id,omitempty"`
+	UserID    int       `json:"user_id,omitempty"`
+	Name      string    `json:"name"`
+	CreatedOn time.Time `json:"-"`
 }
 
 type Readers []Reader
@@ -576,7 +577,7 @@ func (ds *DataStore) GetUserReaders(userID int) ([]Reader, error) {
 func (ds *DataStore) GetReaderByID(readerID int) (Reader, error) {
 
 	query := `SELECT reader_id, user_id, name FROM readers WHERE reader_id = ?`
-	ds.L.Printf("Query: %s [%d]", query, readerID)
+	//ds.L.Printf("Query: %s [%d]", query, readerID)
 
 	var err error
 	row := ds.DB.QueryRow(query, readerID)
@@ -873,6 +874,41 @@ func (ds *DataStore) GetGroupByID(groupID int) (Group, error) {
 	return g, nil
 }
 
+// GetGroupReaders - retrieves a group's members
+func (ds *DataStore) GetGroupReaders(groupID int) ([]Reader, error) {
+	query := `SELECT r.reader_id, r.user_id, r.name, r.created
+		FROM groups g
+		JOIN group_readers gr ON g.id = gr.group_id
+		JOIN readers r ON gr.reader_id = r.reader_id
+		WHERE g.id = ?`
+
+	var rows *sql.Rows
+	var err error
+	readers := []Reader{}
+	rows, err = ds.DB.Query(query, groupID)
+
+	if err != nil {
+		return readers, err
+	}
+	defer rows.Close()
+
+	var r Reader
+	var created string
+
+	for rows.Next() {
+		err := rows.Scan(&r.ID, &r.UserID, &r.Name, &created)
+		if err != nil {
+			return readers, err
+		}
+		//g.UserID, _ = strconv.Atoi(gUserID)
+		r.CreatedOn, _ = time.Parse("2006-01-02T15:04:05Z07:00", created)
+		//g.ID = groupID
+		readers = append(readers, r)
+	}
+
+	return readers, nil
+}
+
 // FindNewGroupsForReader - find groups base on a query
 func (ds *DataStore) FindNewGroupsForReader(q string, readerID int) ([]Group, error) {
 
@@ -896,6 +932,7 @@ func (ds *DataStore) FindNewGroupsForReader(q string, readerID int) ([]Group, er
 	if err != nil {
 		return []Group{}, err
 	}
+	defer rows.Close()
 
 	for rows.Next() {
 		err = rows.Scan(&g.ID, &g.Name, &g.Status)
